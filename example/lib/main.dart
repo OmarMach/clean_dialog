f2890a278040fa529a6f74d3d966762cc3f361c1
omar <omar>
2023-03-24 14:51:23 +0100

    created package

---
+++ example/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:clean_dialog/clean_dialog.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Clean Dialog example app',
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => CleanDialog(
+                    title: 'Success',
+                    content: 'You have successfully updated your information.',
+                    backgroundColor: const Color(0XFF27ae61),
+                    titleTextStyle: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+                    contentTextStyle: const TextStyle(fontSize: 16, color: Colors.white),
+                    actions: [
+                      CleanDialogActionButtons(actionTitle: 'Accept', onPressed: () {}),
+                      CleanDialogActionButtons(
+                        actionTitle: 'Cancel',
+                        onPressed: () => Navigator.pop(context),
+                        textColor: const Color(0XFFbe3a2c),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: const Center(child: Text('First Example')),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => CleanDialog(
+                    title: 'Error',
+                    content: 'We were not able to update your information.',
+                    backgroundColor: const Color(0XFFbe3a2c),
+                    titleTextStyle: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+                    contentTextStyle: const TextStyle(fontSize: 16, color: Colors.white),
+                    actions: [
+                      CleanDialogActionButtons(
+                        actionTitle: 'Cancel',
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      CleanDialogActionButtons(
+                        actionTitle: 'Try again',
+                        textColor: const Color(0XFF27ae61),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: const Center(child: Text('Second Example')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/clean_dialog.dart
+++ lib/clean_dialog.dart
@@ -1,7 +1,113 @@
 library clean_dialog;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CleanDialog extends StatelessWidget {
+  const CleanDialog({
+    super.key,
+    required this.backgroundColor,
+    required this.actions,
+    required this.title,
+    required this.content,
+    this.titleTextStyle,
+    this.contentTextStyle,
+    this.titleTextAlign,
+    this.contentTextAlign,
+  });
+
+  final Color backgroundColor;
+  final TextStyle? titleTextStyle, contentTextStyle;
+  final TextAlign? titleTextAlign, contentTextAlign;
+  final String title, content;
+  final List<CleanDialogActionButtons> actions;
+
+  List<CleanDialogActionButtons> _buildStyledActionButtons() => actions.map((e) => e.copyWith(textColor: e.textColor ?? backgroundColor)).toList();
+
+  @override
+  Widget build(BuildContext context) {
+    final List<CleanDialogActionButtons> actions = _buildStyledActionButtons();
+
+    return Dialog(
+      elevation: 0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: backgroundColor,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        title,
+                        textAlign: titleTextAlign ?? TextAlign.center,
+                        style: titleTextStyle ?? const TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
+                      ),
+                    ),
+                    Text(
+                      content,
+                      textAlign: contentTextAlign ?? TextAlign.center,
+                      style: contentTextStyle ?? const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ListView.separated(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemBuilder: (context, index) => actions.elementAt(index),
+              separatorBuilder: (context, _) => Divider(
+                color: backgroundColor.withOpacity(.5),
+                height: 1,
+              ),
+              itemCount: actions.length,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CleanDialogActionButtons extends StatelessWidget {
+  const CleanDialogActionButtons({super.key, required this.actionTitle, required this.onPressed, this.textColor});
+
+  final String actionTitle;
+  final VoidCallback onPressed;
+  final Color? textColor;
+
+  CleanDialogActionButtons copyWith({
+    String? actionTitle,
+    VoidCallback? onPressed,
+    Color? textColor,
+  }) =>
+      CleanDialogActionButtons(
+        actionTitle: actionTitle ?? this.actionTitle,
+        onPressed: onPressed ?? this.onPressed,
+        textColor: textColor ?? this.textColor,
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: textColor?.withOpacity(.1),
+      highlightColor: textColor?.withOpacity(.2),
+      onTap: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Text(
+          actionTitle,
+          textAlign: TextAlign.center,
+          style: TextStyle(color: textColor ?? Colors.blue, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
 }
